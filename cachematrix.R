06217521738a2b6f4eb16735dc2502dedcbb6cbf
rkenski <rafael.kenski@gmail.com>
2015-02-18 18:51:16 -0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+### As far as I could understand, this exercise required minimal changes to the original code provided. I simply
+### adapted it to work with matrices (instead of vectors) and return inverses (instead of means)..
 
-## Write a short comment describing this function
+###  The following function creates a list of functions for setting ("set") and getting ("get") the value of 
+###  a matrix in a cached environment, and also setting and getting the value of the calculated inverse 
+###  ("setinverse" and "getinverse").
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+### The following function retrieves the values of the matrix or of its inverse in the cached environment created
+### through the makeCacheMatrix () function. It first checks if there is a calculated inverse. In case there is,
+### it returns the cached value. In case there isn't 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse() #### Retrieves the cached inverse (if it exists)  
+  if(!is.null(m)) { #### Checks if the cached inverse is null. If it is not, return the cached value.
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get() ### In case the cached inverse is null, get the original matrix
+  m <- solve(data, ...) ### Find the inverse of the original matrix
+  x$setinverse(m) ### Set the inverse as the calculated value.
+  return(m)
+}
 }
